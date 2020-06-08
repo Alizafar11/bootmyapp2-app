5a492c91c9d89aa64b220085b279c1af7d39748d
alirazaaaa <Alizafar11@users.noreply.github.com>
2020-06-07 21:54:31 -0700

    parent child create props

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Dinner from './Dinner.js';
 function App() {
   return (
     <div className="App" >
-       <Dinner />
+       <Dinner dishName="nihari" dishsweet="kheer" />
        <h1 align="left">ali zafar</h1>
        <p>asdmlkasndklajksbdklansdk</p>
    
--- src/Dinner.js
+++ src/Dinner.js
@@ -1,10 +1,10 @@
 import React from 'react' ;
 
-function Dinner () {
+function Dinner (props) {
     return (
         <div>
-            <h1>diner in we have chicken biryani</h1>
-            <h1>diner in we have chicken biryani</h1>
+            <h1>today we are serving {props.dishName}</h1>
+            <h1>today we are serving {props.dishsweet}</h1>
             <h1>diner in we have chicken biryani</h1>
         </div>
     )
